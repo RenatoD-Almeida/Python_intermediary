0b67e4a2151d34be611e07f26c2e5e3ae7b71e9d
Renato Mateus <marks_renato00@protonmail.com>
2022-04-01 16:22:06 -0300

    Poo python atualizado 01/04

--- POO_1.py
+++ POO_1.py
@@ -1,19 +1,30 @@
 class Felino:
-    def __init__(self, tipo = "<Não informado>", nome = "<Não informado>", andar = False):
+    def __init__(self, tipo = "<Não informado>", nome = "<Não informado>", andar = False, comer = False):
         self.nome = nome
         self.tipo_animal = tipo
         self.andando = andar
+        self.comendo = comer
 
     def status(self):
         print(f'Nome: {self.nome}')
         print(f'tipo: {self.tipo_animal}')
         print(f'Andando') if self.andando else print("Parado")
+        print(f'Se alimentando') if self.comendo else print("Não está se alimentando")
 
     def set_andar(self):
         if self.andando:
             self.andando = False
         else:
             self.andando = True
+
+    def set_comer(self):
+        if self.andando:
+            print("Não pode se alimentar andando")
+        else:
+            if self.comendo:
+                self.comendo = False
+            else:
+                self.comendo = True
 
 
 nome = input("Digite o nome do seu felino: ")
@@ -23,8 +34,13 @@ teste = Felino(tipo, nome)
 teste.status()
 print("+" * 10)
 teste.set_andar()
+teste.set_comer()
 teste.status()
 print("+" * 10)
 teste.set_andar()
 teste.status()
+print("+" * 10)
+teste.set_comer()
+teste.status()
+
 
